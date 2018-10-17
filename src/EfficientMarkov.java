d11392247308f281c735441fa182aad6ab9f7391
mhk29 <42727181+mhk29@users.noreply.github.com>
2018-10-17 01:44:12 -0400

    Add files via upload

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -6,7 +6,7 @@ public class EfficientMarkov extends BaseMarkov {
 	// myText is used for text generation and 
 	// base Markov chooses a random starting
 	
-	HashMap<String, ArrayList<String>> myMap = new HashMap<String, ArrayList<String>>();
+	private HashMap<String, ArrayList<String>> myMap = new HashMap<String, ArrayList<String>>();
 	
 	public EfficientMarkov(int order) {
 		super(order);
@@ -20,30 +20,41 @@ public class EfficientMarkov extends BaseMarkov {
 	@Override
 	public void setTraining(String text) {
 		myText = text;
-		myMap = new HashMap<String, ArrayList<String>>();
 		
-		for (int a = 0; a < text.length() - getOrder() - 1; a++) {
+		for (int a = 0; a < text.length() - getOrder() + 1; a++) {
 			
 			String trainer = myText.substring(a, a + getOrder());
 			ArrayList<String> theOne = new ArrayList<String>();
 			String theAdd;
 			
+			if (a == text.length() - getOrder()) {
+				theAdd = PSEUDO_EOS;
+			}	
+			
+			else {
+				theAdd = myText.substring(a + getOrder(), a + getOrder() + 1);
+			}
+			
 			if (! myMap.containsKey(trainer)) {	
 				myMap.put(trainer, new ArrayList<String>());
-			}		
+			}
 			theOne = myMap.get(trainer);
-			theAdd = myText.substring(a + getOrder(), a + getOrder() + 1);
 			theOne.add(theAdd);
 			myMap.put(trainer, theOne);
+			}
 		}
-		String addLast = myText.substring(myText.length() - getOrder() - 1, myText.length() - 1);
-		if (! myMap.containsKey(addLast)) {	
-			myMap.put(addLast, new ArrayList<String>());
-		}		
-		ArrayList<String> meIn = myMap.get(addLast);
-		meIn.add(PSEUDO_EOS);
-		myMap.put(addLast, meIn);
-	}
+//		String addLast = myText.substring(myText.length() - getOrder() - 1, myText.length() - 1);
+//		if (! myMap.containsKey(addLast)) {	
+//			myMap.put(addLast, new ArrayList<String>());
+//			ArrayList<String> meIn = new ArrayList<String>();
+//			meIn.add(PSEUDO_EOS);
+//			myMap.put(addLast, meIn);
+//		}		
+//		if (myMap.containsKey(addLast)) {	
+//			myMap.get(addLast).add(PSEUDO_EOS);
+//		}	
+		
+	
 
 	
 	@Override
