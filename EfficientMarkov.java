2f4b1e1325d24f2d0c0fd237c24012b837b9e24f
mhk29 <42727181+mhk29@users.noreply.github.com>
2018-10-17 00:57:07 -0400

    Add files via upload

---
+++ EfficientMarkov.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class EfficientMarkov extends BaseMarkov {
+	// myText is used for text generation and 
+	// base Markov chooses a random starting
+	
+	private HashMap<String, ArrayList<String>> myMap = new HashMap<String, ArrayList<String>>();
+	
+	public EfficientMarkov(int order) {
+		super(order);
+	}
+	
+	public EfficientMarkov() {
+		this(3);
+	}
+	
+	
+	@Override
+	public void setTraining(String text) {
+		myText = text;
+		myMap = new HashMap<String, ArrayList<String>>();
+		
+		for (int a = 0; a < text.length() - getOrder() - 1; a++) {
+			
+			String trainer = myText.substring(a, a + getOrder());
+			ArrayList<String> theOne = new ArrayList<String>();
+			String theAdd;
+			
+			if (! myMap.containsKey(trainer)) {	
+				myMap.put(trainer, new ArrayList<String>());
+			}		
+			theOne = myMap.get(trainer);
+			theAdd = myText.substring(a + getOrder(), a + getOrder() + 1);
+			theOne.add(theAdd);
+			myMap.put(trainer, theOne);
+		}
+		String addLast = myText.substring(myText.length() - getOrder() - 1, myText.length() - 1);
+		if (! myMap.containsKey(addLast)) {	
+			myMap.put(addLast, new ArrayList<String>());
+		}		
+		ArrayList<String> meIn = myMap.get(addLast);
+		meIn.add(PSEUDO_EOS);
+		myMap.put(addLast, meIn);
+	}
+
+	
+	@Override
+	public ArrayList<String> getFollows(String key){ 
+		if (! myMap.containsKey(key)) {
+			throw new NoSuchElementException (key+" not in map");
+		}
+		return myMap.get(key);
+	}
+	
+}
---
+++ EfficientWordMarkov.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class EfficientWordMarkov extends BaseWordMarkov {
+	
+	HashMap<WordGram, ArrayList<String>> myMap = new HashMap<WordGram, ArrayList<String>>();
+	
+	public EfficientWordMarkov(int order) {
+		super(order);
+	}
+	
+	public EfficientWordMarkov() {
+		this(3);
+	}
+
+	@Override
+	public void setTraining(String text) {
+		String[] myText = text.split("\\s");
+		myMap = new HashMap<WordGram, ArrayList<String>>();
+		
+		for (int a = 0; a < text.length() - getOrder() - 1; a++) {
+			
+			WordGram trainer = new WordGram(myText, a, getOrder());
+			ArrayList<String> theOne = new ArrayList<String>();
+			String theAdd;
+			
+			if (! myMap.containsKey(trainer)) {	
+				myMap.put(trainer, new ArrayList<String>());
+			}		
+			theOne = myMap.get(trainer);
+			theAdd = myText[a + getOrder()];
+			theOne.add(theAdd);
+			myMap.put(trainer, theOne);
+		}
+		WordGram addLast = new WordGram(myText, text.length() - getOrder(), getOrder());
+		if (! myMap.containsKey(addLast)) {	
+			myMap.put(addLast, new ArrayList<String>());
+		}		
+		ArrayList<String> meIn = myMap.get(addLast);
+		meIn.add(PSEUDO_EOS);
+		myMap.put(addLast, meIn);
+
+		
+	}
+	
+}
