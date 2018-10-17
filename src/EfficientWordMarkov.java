10a9a61b01a0a0023ad41eb78535a1c62fa284c8
mhk29 <42727181+mhk29@users.noreply.github.com>
2018-10-17 02:11:20 -0400

    Add files via upload

--- src/EfficientWordMarkov.java
+++ src/EfficientWordMarkov.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 
 public class EfficientWordMarkov extends BaseWordMarkov {
 	
-	HashMap<WordGram, ArrayList<String>> myMap = new HashMap<WordGram, ArrayList<String>>();
+	private HashMap<WordGram, ArrayList<String>> myMap = new HashMap<WordGram, ArrayList<String>>();
 	
 	public EfficientWordMarkov(int order) {
 		super(order);
@@ -18,29 +19,45 @@ public class EfficientWordMarkov extends BaseWordMarkov {
 		String[] myText = text.split("\\s");
 		myMap = new HashMap<WordGram, ArrayList<String>>();
 		
-		for (int a = 0; a < text.length() - getOrder() - 1; a++) {
+		for (int a = 0; a < myText.length - getOrder() + 1; a++) {
 			
 			WordGram trainer = new WordGram(myText, a, getOrder());
 			ArrayList<String> theOne = new ArrayList<String>();
 			String theAdd;
 			
+		
+			
+			if (a == myText.length - getOrder()) {
+				theAdd = PSEUDO_EOS;
+			}
+			else {
+				theAdd = myText[a + getOrder()];
+			}
+			
 			if (! myMap.containsKey(trainer)) {	
 				myMap.put(trainer, new ArrayList<String>());
-			}		
+			}
+			
 			theOne = myMap.get(trainer);
-			theAdd = myText[a + getOrder()];
 			theOne.add(theAdd);
 			myMap.put(trainer, theOne);
 		}
-		WordGram addLast = new WordGram(myText, text.length() - getOrder(), getOrder());
-		if (! myMap.containsKey(addLast)) {	
-			myMap.put(addLast, new ArrayList<String>());
-		}		
-		ArrayList<String> meIn = myMap.get(addLast);
-		meIn.add(PSEUDO_EOS);
-		myMap.put(addLast, meIn);
+//		WordGram addsLast = new WordGram(myText, text.length() - getOrder(), getOrder());
+//		if (! myMap.containsKey(addLast)) {	
+//			myMap.put(addLast, new ArrayList<String>());
+//		}		
+//		ArrayList<String> meIn = myMap.get(addLast);
+//		meIn.add(PSEUDO_EOS);
+//		myMap.put(addLast, meIn);
 
 		
 	}
 	
+	@Override
+	public ArrayList<String> getFollows(WordGram key) {
+		if (! myMap.containsKey(key)) {
+			throw new NoSuchElementException (key+" not in map");
+		}
+		return myMap.get(key);
+	}
 }
